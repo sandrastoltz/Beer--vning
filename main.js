238c7038672e7d7428fc204b063a43cb644c5cbf
cozyhorse <mo.apiban@gmail.com>
2023-11-22 11:48:34 +0100

    added cards and css

--- main.js
+++ main.js
@@ -3,6 +3,24 @@
 //länka "see more" till ölens "info" och skriv ut det i en lista
 // slumpas varje gång sidan laddar om
 //En sökfunktion för att hitta öl och skriver ut en lista med max 10st items som är knappar som länkar till "see more"
-//
 //skapa ett kort i html som kan uppdateras med data från API. skall innehålla namnet på ölen & "see more"
-// see more page med inf
+//hämtar en lista på öl
+
+const beerImg = document.querySelector(".main__beer-image");
+
+//Hämtar alla öl
+const getBeer = async () => {
+    const response = await fetch("https://api.punkapi.com/v2/beers/")
+    const data = await response.json();
+    console.log("beers", data);
+
+    
+}
+
+//Hämtar en random öl
+const getRandomBeer = async () => {
+    const response = await fetch("https://api.punkapi.com/v2/beers/random/")
+    const data = await response.json();
+    console.log("random beer", data); 
+}
+
