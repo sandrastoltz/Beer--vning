519548f87aa74b4719f7beea97bdcc3eda74108f
cozyhorse <mo.apiban@gmail.com>
2023-11-22 11:09:09 +0100

    init

---
+++ main.js
@@ -0,0 +1,8 @@
+//Funktion som fetchar öl från ett API
+//Lägger in bild och text i ett kort utifrån API
+//länka "see more" till ölens "info" och skriv ut det i en lista
+// slumpas varje gång sidan laddar om
+//En sökfunktion för att hitta öl och skriver ut en lista med max 10st items som är knappar som länkar till "see more"
+//
+//skapa ett kort i html som kan uppdateras med data från API. skall innehålla namnet på ölen & "see more"
+// see more page med inf
